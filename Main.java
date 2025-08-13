9b8972aad1d7b8b4cf4952f75a9e192f4f542bf1
joaoferretti1 <jfcarvalho@minha.edu.br>
2025-08-13 19:22:58 -0300

    Feito Hola que tal

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-     }
+        olaMundo();
+        Hola();
     }
+
     public static void olaMundo() {
-    System.out.println("Olá Mundo");
+        System.out.println("Olá mundo");
     }
 
-    
-
-    
+    public static void Hola() {
+        System.out.println("Hola que tal");
+    }
+}
